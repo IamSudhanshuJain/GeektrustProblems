1e748172080c9bc2c367478c80c28c2f85689b6d
Sudhanshu_Jain <jainsudhanshu10@gmail.com>
2021-12-05 15:51:10 +0530

    Upload my money solution (#1)
    * Add files via upload
    
    * Create ReadMe.md
    
    * commit the changes
    
    * commit
    
    * commit the mymoney solution
    
    * remove the geektrust folder and change the dependencies
    
    * commit the tst
    
    Co-authored-by: Sudhanshu Jain <sudhanshu.jain@globallogic.com>

---
+++ MyMoney/MyMoneySolution/Constants/InputCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace MyMoneySolution.Constants
+{
+    public static class InputCommand
+    {
+        public const string ALLOCATE = "ALLOCATE";
+        public const string SIP = "SIP";
+        public const string CHANGE = "CHANGE";
+        public const string BALANCE = "BALANCE";
+        public const string REBALANCE = "REBALANCE";
+    }
+}
---
+++ MyMoney/MyMoneySolution/InvestmentExtensions.cs
@@ -0,0 +1,11 @@
+﻿using MyMoneySolution.Constants;
+namespace MyMoneySolution
+{
+    public static class InvestmentExtensions
+    {
+        internal static bool CheckRebalanceMonth(this Month month)
+        {
+            return month == Month.JUNE || month == Month.DECEMBER; 
+        }
+    }
+}
